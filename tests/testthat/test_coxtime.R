f7a202766bc7e6d8884b03df71af2b4132ff3f7d
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-09-24 21:11:30 +0100

    add skip_on_os for python

--- tests/testthat/test_coxtime.R
+++ tests/testthat/test_coxtime.R
@@ -1,4 +1,5 @@
 test_that("silent", {
+  skip_on_os("windows")
   expect_message({fit <- coxtime(Surv(time, status) ~ ., data = rats[1:50, ], verbose = FALSE)})
   expect_silent({predict(fit, newdata = rats[51:100, ])})
 })
--- tests/testthat/test_dnnsurv.R
+++ tests/testthat/test_dnnsurv.R
@@ -1,4 +1,5 @@
 test_that("silent", {
+  skip_on_os("windows")
   expect_silent({fit <- dnnsurv(Surv(time, status) ~ ., data = rats[1:50, ], verbose = FALSE)})
   expect_silent({predict(fit, newdata = rats[51:100, ])})
 })
