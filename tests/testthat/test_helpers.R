0e883abb7cee1262c93609e71e204e0ff40387be
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-09-28 10:44:11 +0100

    remove knitr dependency

--- tests/testthat/test_helpers.R
+++ tests/testthat/test_helpers.R
@@ -18,13 +18,3 @@ test_that("clean_test_data", {
   expect_equal(clean_test_data(fit), fit$x)
   expect_error(clean_test_data(fit, rats[, 1:2]), "Names in")
 })
-
-test_that("get_keras_optimizer", {
-  expect_is(get_keras_optimizer("adadelta"), "keras.optimizer_v2.adadelta.Adadelta")
-  expect_is(get_keras_optimizer("adagrad"), "keras.optimizer_v2.adagrad.Adagrad")
-  expect_is(get_keras_optimizer("adamax"), "keras.optimizer_v2.adamax.Adamax")
-  expect_is(get_keras_optimizer("adam"), "keras.optimizer_v2.adam.Adam")
-  expect_is(get_keras_optimizer("nadam"), "keras.optimizer_v2.nadam.Nadam")
-  expect_is(get_keras_optimizer("rmsprop"), "keras.optimizer_v2.rmsprop.RMSprop")
-  expect_is(get_keras_optimizer("sgd"), "keras.optimizer_v2.gradient_descent.SGD")
-})
