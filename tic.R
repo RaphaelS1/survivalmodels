e73cce99e3ec821024e8c63f9e0e5b5df590b5e9
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-10-21 11:45:16 +0100

    Update tic.R

--- tic.R
+++ tic.R
@@ -2,6 +2,10 @@
 do_package_checks()
 
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
+  
+get_stage("before_deploy") %>%
+  add_step(step_setup_push_deploy(branch = "master", orphan = FALSE))
+  
   get_stage("deploy") %>%
     add_code_step(rmarkdown::render("README.Rmd")) %>%
     add_step(step_do_push_deploy(commit_paths = c("README.md")))
