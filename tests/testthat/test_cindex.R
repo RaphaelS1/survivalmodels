0db846e07793148b53d7cfdee9b91fdba5238a19
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2021-01-17 11:11:02 +0000

    fix cindex tests

--- tests/testthat/test_cindex.R
+++ tests/testthat/test_cindex.R
@@ -1,13 +1,7 @@
 skip_if_not_installed("survival")
 
 test_that("cindex", {
-  set.seed(10)
-  np <- reticulate::import("numpy")
-  np$random$seed(1L)
-  torch <- reticulate::import("torch")
-  torch$manual_seed(1L)
-  data <- simsurvdata(20)
-  fit <- deepsurv(data = data[1:10, ])
-  p <- predict(fit, type = "risk", newdata = data[11:20, ])
-  expect_true(cindex(risk = p, truth = data[11:20, "time"]) >= 0.5)
+  expect_equal(cindex(1:10, 10:1), 1)
+  expect_equal(cindex(1:10, 1:10), 0)
+  expect_error(cindex(1:5, 1:6), "length")
 })
