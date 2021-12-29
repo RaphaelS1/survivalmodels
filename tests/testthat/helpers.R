7fe22514af6ba968d3dda0424eb375af0db280d7
RaphaelS1 <raphaelsonabend@gmail.com>
2021-12-29 12:14:10 +0000

    make sanity easier

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -9,30 +9,28 @@ skip_if_no_pycox <- function() {
 }
 
 sanity_check <- function(model, pars) {
-  try_again(3, {
-    skip_if_not_installed("distr6")
+  skip_if_not_installed("distr6")
 
-    set.seed(42)
-    if (model != "akritas") {
-      set_seed(42)
-    }
+  set.seed(42)
+  if (model != "akritas") {
+    set_seed(42)
+  }
 
-    train <- simsurvdata(200, cens = 0.2)
-    test <- simsurvdata(50, cens = 0.2)
+  train <- simsurvdata(300, cens = 0.1)
+  test <- simsurvdata(50, cens = 0.1)
 
-    y <- survival::Surv(test$time, test$status)
+  y <- survival::Surv(test$time, test$status)
 
-    fit <- do.call(
-      get(model),
-      c(list(formula = Surv(time, status) ~ ., data = train), pars)
-    )
+  fit <- do.call(
+    get(model),
+    c(list(formula = Surv(time, status) ~ ., data = train), pars)
+  )
 
-    p <- predict(fit, newdata = test, type = "all", distr6 = TRUE)
+  p <- predict(fit, newdata = test, type = "all", distr6 = TRUE)
 
-    expect_true(cindex(p$risk, y) >= 0.5)
-    expect_equal(length(p$risk), nrow(p$surv$modelTable))
+  expect_true(cindex(p$risk, y) >= 0.5)
+  expect_equal(length(p$risk), nrow(p$surv$modelTable))
 
-    p <- predict(fit, newdata = test, type = "all", distr6 = FALSE)
-    expect_equal(length(p$risk), nrow(p$surv))
-  })
+  p <- predict(fit, newdata = test, type = "all", distr6 = FALSE)
+  expect_equal(length(p$risk), nrow(p$surv))
 }
