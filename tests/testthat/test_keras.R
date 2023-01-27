b4b50ba3f53d673002d06a7b5111644165cc2973
RaphaelS1 <raphaelsonabend@gmail.com>
2023-01-27 17:11:44 +0000

    fix and test on windows

--- tests/testthat/test_keras.R
+++ tests/testthat/test_keras.R
@@ -1,11 +1,12 @@
-skip_on_os("windows")
+# skip_on_os("windows")
 skip_if_not_installed("keras")
 
 test_that("inherits(get_keras_optimizer", {
+  library(keras)
   for (opt in survivalmodels:::keras_optimizers) {
     expect_equal(
       class(get_keras_optimizer(opt)),
-      do.call(sprintf("keras::optimizer_%s", opt), list())
+      class(do.call(sprintf("optimizer_%s", opt), list()))
     )
   }
 })
